5790df26cc580a28863dbe626e397361033d4b64
M4rquinhos <marco.37099p@gmail.com>
2022-02-10 19:13:18 -0600

    assd

--- CURSO JAVASCRIPT/leccion 02/UsoBasico-var_let_const.js
+++ CURSO JAVASCRIPT/leccion 02/UsoBasico-var_let_const.js
@@ -20,11 +20,24 @@ const apellido = "Perez";
 
 
 // Buenas practicas para nombres de variables
-let nombreCompleto = "Juan Perez";
-console.log(nombreCompleto);
+// let nombreCompleto = "Juan Perez";
+// console.log(nombreCompleto);
 
 let x, y;
 x = 10;
 y = 20;
 let z = x + y;
 console.log(z);
+
+
+// Regglas para definir nombres de variables 
+let nombreCompleto = "Juan Perez";
+let nombrecompleto = "Carlos Gonzalez";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+// 1. No puede comenzar con numeros
+// 2. No puede contener espacios
+// 3. No puede contener caracteres especiales excepto $nombreDeVariable
+// 4. No puede contener guiones bajos
+// 5. No puede contener guiones medios
